7caae3fe4c0c68ad013e35ade55f48a96d358799
jclagoria <lagoria79@gmail.com>
2024-02-14 15:55:10 -0300

    Add changes, add Classes for test

--- src/test/java/ar/com/api/derivatives/services/DerivativesGeckoApiServiceTest.groovy
+++ src/test/java/ar/com/api/derivatives/services/DerivativesGeckoApiServiceTest.groovy
@@ -1,16 +1,18 @@
 package ar.com.api.derivatives.services
 
 import ar.com.api.derivatives.configuration.ExternalServerConfig
-import ar.com.api.derivatives.exception.ManageExceptionCoinGeckoServiceApi
+import ar.com.api.derivatives.dto.ExchangeIdDTO
 import ar.com.api.derivatives.exception.ServiceException
 import ar.com.api.derivatives.model.Derivative
+import ar.com.api.derivatives.model.DerivativeData
 import ar.com.api.derivatives.model.Exchange
+import ar.com.api.derivatives.utils.ValidationUtils
 import org.instancio.Instancio
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
-import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Stepwise
 
@@ -31,7 +33,8 @@ class DerivativesGeckoApiServiceTest extends Specification {
 
         externalServerConfig.getUrlCoinGecko() >> "mockUrlGlobal"
         externalServerConfig.getDerivativesExchangesList() >> "mockUriListDerivatives"
-        externalServerConfig.getDerivativesGecko() >> "mockUriDerivatives"
+        externalServerConfig.getDerivativesGecko() >> "mockUriDerivativesGecko"
+        externalServerConfig.getDerivativesExchangesByIdGecko() >> "mockUriDerivativeData"
 
         webClientMock.get() >> requestHeaderUriMock
         requestHeaderUriMock.uri(_ as String) >> requestHeadersMock
@@ -58,38 +61,24 @@ class DerivativesGeckoApiServiceTest extends Specification {
 
     def "getListOfDerivatives handles 400 Bad Request Error"() {
         given:
-            simulateWebClientErrorResponse(400, "Bad Request", Derivative)
+            simulateWebClientErrorResponseForFlux(400, "Bad Request", Derivative)
 
         when:
-            Flux<Exchange> result = derivativesGeckoApiServiceMock.getListOfDerivatives()
+            Flux<Exchange> result4xxError = derivativesGeckoApiServiceMock.getListOfDerivatives()
 
         then:
-            StepVerifier.create(result)
-                    .expectErrorMatches(
-                            throwable ->
-                                    throwable instanceof ServiceException &&
-                                            throwable.getCause() instanceof WebClientResponseException &&
-                                            ((WebClientResponseException) throwable.getCause())
-                                                    .getStatusCode().is4xxClientError())
-                    .verify()
+            ValidationUtils.validate4xxError(result4xxError)
     }
 
     def "getListOfDerivatives handles 500 Internal Server Exception"() {
         given:
-            simulateWebClientErrorResponse(500, "Internal Server Exception", Derivative)
+            simulateWebClientErrorResponseForFlux(500, "Internal Server Exception", Derivative)
 
         when:
             Flux<Exchange> error5xxResult = derivativesGeckoApiServiceMock.getListOfDerivatives()
 
         then:
-            StepVerifier.create(error5xxResult)
-                    .expectErrorMatches {throwable ->
-                        throwable instanceof ServiceException &&
-                                throwable.getCause() instanceof  WebClientResponseException &&
-                                ((WebClientResponseException) throwable.getCause())
-                                        .getStatusCode().is5xxServerError()
-                    }
-                    .verify()
+            ValidationUtils.validate5xxError(error5xxResult)
     }
 
     def "getListOfDerivativesExchanges returns a list of exchanges successfully"() {
@@ -109,35 +98,68 @@ class DerivativesGeckoApiServiceTest extends Specification {
 
     def "getListOfDerivativesExchanges handles 400 Bad Request Error"() {
         given:
-        simulateWebClientErrorResponse(400, "Bad Request", Exchange)
+            simulateWebClientErrorResponseForFlux(400, "Bad Request", Exchange)
 
         when:
-        Flux<Exchange> result4xxError = derivativesGeckoApiServiceMock.getListOfDerivativesExchanges()
+            Flux<Exchange> result4xxError = derivativesGeckoApiServiceMock.getListOfDerivativesExchanges()
 
         then:
-        StepVerifier.create (result4xxError).expectErrorMatches (
-                throwable -> throwable instanceof ServiceException &&
-                        throwable.getCause() instanceof WebClientResponseException &&
-                        ((WebClientResponseException) throwable.getCause()).getStatusCode().is4xxClientError()
-        ).verify()
+            ValidationUtils.validate4xxError(result4xxError)
     }
 
     def "getListOfDerivativesExchanges handles 500 Internal Server Error"() {
         given:
-        simulateWebClientErrorResponse(500, "Internal Server Error", Exchange)
+            simulateWebClientErrorResponseForFlux(500, "Internal Server Error", Exchange)
 
         when:
-        Flux<Exchange> result4xxError = derivativesGeckoApiServiceMock.getListOfDerivativesExchanges()
+            Flux<Exchange> result5xxError = derivativesGeckoApiServiceMock.getListOfDerivativesExchanges()
 
         then:
-        StepVerifier.create (result4xxError).expectErrorMatches (
-                throwable -> throwable instanceof ServiceException &&
-                        throwable.getCause() instanceof WebClientResponseException &&
-                        ((WebClientResponseException) throwable.getCause()).getStatusCode().is5xxServerError()
-        ).verify()
+            ValidationUtils.validate5xxError(result5xxError)
     }
 
-    private void simulateWebClientErrorResponse(int statusCode, String statusMessage, Class responseType) {
+    def "getShowDerivativeExchangeData returns derivative data successfully"() {
+        given:
+            ExchangeIdDTO filterDTO = Instancio.create(ExchangeIdDTO)
+            DerivativeData expectedDerivativeData = Instancio.create(DerivativeData)
+            responseSpecMock.bodyToMono(DerivativeData) >> Mono.just(expectedDerivativeData)
+
+        when:
+            Mono<DerivativeData> actualDerivativeData = derivativesGeckoApiServiceMock
+                    .getShowDerivativeExchangeData(filterDTO)
+
+        then:
+            StepVerifier
+                    .create(actualDerivativeData)
+                    .expectNext(expectedDerivativeData)
+                    .verifyComplete()
+    }
+
+    def "getShowDerivativeExchangeData handles 400 Bad Request Error"() {
+        given:
+            ExchangeIdDTO filterDTO = Instancio.create(ExchangeIdDTO)
+            simulateWebClientErrorResponseForMono(400, "Bad Request", DerivativeData)
+
+        when:
+        Mono<DerivativeData> result4xxError = derivativesGeckoApiServiceMock.getShowDerivativeExchangeData(filterDTO)
+
+        then:
+            ValidationUtils.validate4xxError(result4xxError)
+    }
+
+    def "getShowDerivativeExchangeData handles 500 Internal Server Error"() {
+        given:
+            ExchangeIdDTO filterDTO = Instancio.create(ExchangeIdDTO)
+            simulateWebClientErrorResponseForMono(500, "Internal Server Error", DerivativeData)
+
+        when:
+            Mono<DerivativeData> result5xxError = derivativesGeckoApiServiceMock.getShowDerivativeExchangeData(filterDTO)
+
+        then:
+            ValidationUtils.validate5xxError(result5xxError)
+    }
+
+    private void simulateWebClientErrorResponseForFlux(int statusCode, String statusMessage, Class responseType) {
         WebClient.ResponseSpec errorResponseSpecMock = Mock()
         WebClient.RequestHeadersSpec requestHeadersSpecMock = Mock()
         WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock = Mock()
@@ -149,7 +171,16 @@ class DerivativesGeckoApiServiceTest extends Specification {
         responseSpecMock.bodyToFlux(responseType) >> Flux.error(mockException)
     }
 
-
-
+    private void simulateWebClientErrorResponseForMono(int statusCode, String statusMessage, Class responseType) {
+        WebClient.ResponseSpec errorResponseSpecMock = Mock()
+        WebClient.RequestHeadersSpec requestHeadersSpecMock = Mock()
+        WebClient.RequestHeadersUriSpec requestHeadersUriSpecMock = Mock()
+        webClientMock.get() >> requestHeadersUriSpecMock
+        requestHeadersUriSpecMock.uri(_) >> requestHeadersSpecMock
+        requestHeadersSpecMock.retrieve() >> errorResponseSpecMock
+        WebClientResponseException mockException = WebClientResponseException
+                .create(statusCode, statusMessage, null, null, null)
+        responseSpecMock.bodyToMono(responseType) >> Mono.error(mockException)
+    }
 
 }
